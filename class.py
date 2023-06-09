9e209349289a2552c826ac5095d127aa1d00497c
color8892 <84972148+color8892@users.noreply.github.com>
2023-06-09 21:23:23 +0800

    Update class.py

--- class.py
+++ class.py
@@ -9,20 +9,36 @@ def resize_images(image_folder, output_folder):
         class_folder = os.path.join(output_folder, f'class_{i}')
         os.makedirs(class_folder, exist_ok=True)
     images = []
+    filenames = []
     for i, filename in enumerate(sorted(os.listdir(image_folder))):
         if filename.endswith('.jpg') or filename.endswith('.png') or filename.endswith('.jpeg') or filename.endswith('.JPG') or filename.endswith('.PNG') or filename.endswith('.JPEG'):
             img_path = os.path.join(image_folder, filename)
             img = cv2.imread(img_path)
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) 
-            img = cv2.resize(img, (200, 200))  
-            images.append(img.flatten()) 
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            img = cv2.resize(img, (200, 200))
+            cv2.imwrite(os.path.join(output_folder, f'{filename[:-4]}_original.png'), img)
 
-            resized_img_path = os.path.join(output_folder, filename)
-            cv2.imwrite(resized_img_path, cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
+            # Save and append the horizontally flipped image
+            img_flipped = cv2.flip(img, 1)
+            cv2.imwrite(os.path.join(output_folder, f'{filename[:-4]}_flipped.png'), img_flipped)
+            images.append(img_flipped.flatten())
 
-            flipped_img = cv2.flip(img, 1)  
-            flipped_img_path = os.path.join(class_folder, f'flipped_{filename}')
-            cv2.imwrite(flipped_img_path, cv2.cvtColor(flipped_img, cv2.COLOR_RGB2BGR))
+            # Save and append the 90 degree rotated image
+            img_rotated_90 = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
+            cv2.imwrite(os.path.join(output_folder, f'{filename[:-4]}_rotated_90.png'), img_rotated_90)
+            images.append(img_rotated_90.flatten())
+
+            # Save and append the 180 degree rotated image
+            img_rotated_180 = cv2.rotate(img, cv2.ROTATE_180)
+            cv2.imwrite(os.path.join(output_folder, f'{filename[:-4]}_rotated_180.png'), img_rotated_180)
+            images.append(img_rotated_180.flatten())
+
+            # Save and append the 270 degree rotated image
+            img_rotated_270 = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
+            cv2.imwrite(os.path.join(output_folder, f'{filename[:-4]}_rotated_270.png'), img_rotated_270)
+            images.append(img_rotated_270.flatten())
+
+            filenames.append([f'{filename[:-4]}_original.png', f'{filename[:-4]}_flipped.png', f'{filename[:-4]}_rotated_90.png', f'{filename[:-4]}_rotated_180.png', f'{filename[:-4]}_rotated_270.png'])
 
     data = np.array(images)
 
@@ -31,11 +47,16 @@ def resize_images(image_folder, output_folder):
 
     labels = kmeans.labels_
 
-    for i, label in enumerate(labels):
-        img_path = os.path.join(output_folder, sorted(os.listdir(image_folder))[i])  # 使用原图像的名称
+    for i in range(0, len(labels), 5):
+        label = labels[i]
         class_folder = os.path.join(output_folder, f'class_{label}')
-
-        shutil.move(img_path, class_folder)
+        for j in range(5):
+            filename = filenames[i // 5][j]
+            src_path = os.path.join(output_folder, filename)
+            dst_path = os.path.join(class_folder, filename)
+            if not os.path.exists(src_path) or os.path.exists(dst_path):
+                continue
+            shutil.move(src_path, dst_path)
 
 
 image_folder_car = 'C:/Users/mnb35/Downloads/PYTHON/SIGN/CAR'
@@ -45,13 +66,3 @@ resize_images(image_folder_car, output_folder_car)
 image_folder_test = 'C:/Users/mnb35/Downloads/PYTHON/SIGN/TEST'
 output_folder_test = 'C:/Users/mnb35/Downloads/PYTHON/SIGN/TEST_CLASSIFIED'
 resize_images(image_folder_test, output_folder_test)
-
-
-#It defines a function resize_images that takes an image folder path and an output folder path as input.
-#It creates subfolders in the output folder for different classes (in this case, 3 classes).
-#It loads image data from the input folder, resizes the images to a specified size (200x200), and flattens them into 1D vectors.
-#It saves the resized images and their horizontally flipped versions to the output folder.
-#It converts the image data into a NumPy array.
-#It creates a K-Means clustering model and fits the data.
-#It assigns cluster labels to the images.
-#It moves the image files to their respective class folders based on the assigned labels.
