47cf9cd82b4fc833bcc3abebe69b61c1ed365321
color8892 <84972148+color8892@users.noreply.github.com>
2023-06-09 21:49:17 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,105 +1,63 @@
-import torch
-from torch import nn
-from torch.nn import functional as F
-from torchvision import datasets, transforms
-from torch.utils.data import DataLoader
+import os
+import cv2
+import numpy as np
+import shutil
+from sklearn.cluster import KMeans
 
-batch_size = 32
-learning_rate = 1e-3
-num_epochs = 30
+def resize_images(image_folder, output_folder):
+    for i in range(3): 
+        class_folder = os.path.join(output_folder, f'class_{i}')
+        os.makedirs(class_folder, exist_ok=True)
+    images = []
+    filenames = []
+    for i, filename in enumerate(sorted(os.listdir(image_folder))):
+        if filename.endswith('.jpg') or filename.endswith('.png') or filename.endswith('.jpeg') or filename.endswith('.JPG') or filename.endswith('.PNG') or filename.endswith('.JPEG'):
+            img_path = os.path.join(image_folder, filename)
+            img = cv2.imread(img_path)
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            img = cv2.resize(img, (200, 200))
+            cv2.imwrite(os.path.join(output_folder, f'{filename[:-4]}_original.png'), img)
 
-transform = transforms.Compose([
-    transforms.Grayscale(),
-    transforms.Resize((200, 200)),
-    transforms.ToTensor(),
-])
+            # Save and append the 90 degree rotated image
+            img_rotated_90 = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
+            cv2.imwrite(os.path.join(output_folder, f'{filename[:-4]}_rotated_90.png'), img_rotated_90)
+            images.append(img_rotated_90.flatten())
 
+            # Save and append the 180 degree rotated image
+            img_rotated_180 = cv2.rotate(img, cv2.ROTATE_180)
+            cv2.imwrite(os.path.join(output_folder, f'{filename[:-4]}_rotated_180.png'), img_rotated_180)
+            images.append(img_rotated_180.flatten())
 
-train_dataset = datasets.ImageFolder('C:/Users/mnb35/Downloads/PYTHON/SIGN/CAR_CLASSIFIED', transform=transform)
-test_dataset = datasets.ImageFolder('C:/Users/mnb35/Downloads/PYTHON/SIGN/TEST_CLASSIFIED', transform=transform)
+            # Save and append the 270 degree rotated image
+            img_rotated_270 = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
+            cv2.imwrite(os.path.join(output_folder, f'{filename[:-4]}_rotated_270.png'), img_rotated_270)
+            images.append(img_rotated_270.flatten())
 
-train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
-test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+            filenames.append([f'{filename[:-4]}_original.png', f'{filename[:-4]}_flipped.png', f'{filename[:-4]}_rotated_90.png', f'{filename[:-4]}_rotated_180.png', f'{filename[:-4]}_rotated_270.png'])
 
-#from torch.utils.data import random_split
+    data = np.array(images)
 
-#total_samples = len(train_dataset)
-#split_ratio = 0.2
-#test_samples = int(total_samples * split_ratio)
-#train_samples = total_samples - test_samples
+    kmeans = KMeans(n_clusters=3) 
+    kmeans.fit(data)
 
-#train_dataset, test_dataset = random_split(train_dataset, [train_samples, test_samples])
+    labels = kmeans.labels_
 
-#train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
-#test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
-
-
-class CRNN(nn.Module):
-    def __init__(self):
-        super(CRNN, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
-        self.pool1 = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
-        self.pool2 = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(64*50*50, 128)
-
-        self.lstm = nn.LSTM(input_size=128, hidden_size=64, num_layers=2, batch_first=True, bidirectional=True)
-        self.fc2 = nn.Linear(128, 3)
-
-    def forward(self, x):
-        x = self.pool1(F.relu(self.conv1(x)))
-        x = self.pool2(F.relu(self.conv2(x)))
-        x = x.view(x.size(0), -1)
-        x = F.relu(self.fc1(x))
-        x = x.unsqueeze(1)
-        x, _ = self.lstm(x)
-        x = self.fc2(x.squeeze(1))
-        return x
-
-model = CRNN()
-if torch.cuda.is_available():
-    model = model.cuda()
-
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-
-for epoch in range(num_epochs):
-    for i, (images, labels) in enumerate(train_loader):
-        if torch.cuda.is_available():
-            images = images.cuda()
-            labels = labels.cuda()
-
-        outputs = model(images)
-        loss = criterion(outputs, labels)
-
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-model.eval()
-with torch.no_grad():
-    correct = 0
-    total = 0
-    for images, labels in test_loader:
-        if torch.cuda.is_available():
-            images = images.cuda()
-            labels = labels.cuda()
-
-        outputs = model(images)
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
-
-    print('Test Accuracy: {} %'.format(100 * correct / total))
+    for i in range(0, len(labels), 5):
+        label = labels[i]
+        class_folder = os.path.join(output_folder, f'class_{label}')
+        for j in range(5):
+            filename = filenames[i // 5][j]
+            src_path = os.path.join(output_folder, filename)
+            dst_path = os.path.join(class_folder, filename)
+            if not os.path.exists(src_path) or os.path.exists(dst_path):
+                continue
+            shutil.move(src_path, dst_path)
 
 
-#Set parameters such as batch size, learning rate, and the number of epochs.
-#Define image transformations, including converting images to grayscale, resizing them to 200*200 pixels, and converting them to tensors.
-#Load the training and test datasets using the ImageFolder class and apply the defined transformations.
-#Create data loaders for the training and test datasets to batch and load the data.
-#Define the CRNN (Convolutional Recurrent Neural Network) model class, including convolutional layers, pooling layers, fully connected layers, LSTM layer, and output layer.
-#Instantiate the CRNN model.
-#Define the loss function (cross-entropy loss) and optimizer (Adam optimizer).
-#Train the model by iterating over multiple epochs. For each iteration, retrieve images and labels from the data loader,perform forward pass, compute the loss, backpropagate, and optimize the parameters.
-#Display the current training progress at specific steps (every 100 steps).
-#After training, evaluate the model on the test dataset. Set the model to evaluation mode, disable gradient computation. Iterate over images and labels in the test data loader, make predictions, calculate classification accuracy.
+image_folder_car = 'C:/Users/mnb35/Downloads/PYTHON/SIGN/CAR'
+output_folder_car = 'C:/Users/mnb35/Downloads/PYTHON/SIGN/CAR_CLASSIFIED'
+resize_images(image_folder_car, output_folder_car)
+
+image_folder_test = 'C:/Users/mnb35/Downloads/PYTHON/SIGN/TEST'
+output_folder_test = 'C:/Users/mnb35/Downloads/PYTHON/SIGN/TEST_CLASSIFIED'
+resize_images(image_folder_test, output_folder_test)
